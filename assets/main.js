c9034d57d8529d3f2fdad89fc4c3219bdaed59c4
Soufian <soufian.souilm@gmail.com>
2023-10-19 23:03:42 +0200

    project is ready

---
+++ assets/main.js
@@ -0,0 +1,21 @@
+console.log("it works ");
+
+function adult() {
+  const ageinput = document.getElementById("age");
+
+  if (age >= 18) {
+    console.log("sie sind über18");
+  } else {
+    console.log("sie sind unter 18");
+  }
+}
+
+function wetterqualität() {
+  const wetterqualität = document.getElementById("wetter qualität");
+
+  if (weather <= 10) {
+    console.log("schlecht und kalt");
+  } else {
+    console.log("gut und sonnig");
+  }
+}
